d75e4287e4c5e56ff9b5b2595787987535d114ba
FrancescaPicoco <francesca.picoco33@gmail.com>
2023-11-15 17:10:13 +0100

    done

--- main.js
+++ main.js
@@ -5,8 +5,14 @@ const vueData = {
         return {
             messaggio : "MUSHROOMS",
             image : "img/pexels-ameruverse-digital-marketing-media-1643409.jpg",
-            size : "resize"
+            size : "resize",
+            color: "color-0"
         }     
+    },
+    methods: {
+        clicked:function (){
+            this.color = "color-2"
+        }
     }
 };
 createApp(vueData).mount('#app')
